87deb4b9ccde4c9672bc19c47b41ae65c30d5c3b
andrewkohn <contact@kohnandrew.com>
2024-07-28 13:53:10 -0500

    Add: ts challenge template

---
+++ typescript/template.ts
@@ -0,0 +1,21 @@
+//
+
+// INFO:
+// Submitted:
+// Runtime:
+// Memory:
+
+/* FUNCTION */
+
+/* INPUT */
+
+/* OUTPUT */
+
+console.log(renameThisFunction());
+console.log();
+console.log(renameThisFunction());
+console.log();
+console.log(renameThisFunction());
+console.log();
+
+export { }; // HACK : removes error diag about redeclaring block-scope vars
