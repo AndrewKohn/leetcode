1428a010039c715fd21cb7f6fe507764bf2cfd7a
andrewkohn <contact@kohnandrew.com>
2024-07-29 12:02:59 -0500

    Add: leetcode notes

---
+++ lc-notes.js
@@ -0,0 +1,16 @@
+// TODO : Challenges to retry...
+// 1    -   Runtime: 157ms, beats 12.99%    |
+//          - Have a better function that's less than O(n^2)
+//          - lower runtime speed
+// 151  -   Runtime: 51ms, beats 91.30%     |   Memory: 52.00mb, beats 26.97%
+//          - runtime speed is good, but memory usage is high
+// 238  -   Runtime: 91ms, beats 94.68%     |   Memory: 68.08mb, beats 23.49%
+//          - runtime speed is good, but memory usage is high
+
+// NOTE : Notable challenges...
+// 1071 -   Runtime: 41ms, beats 99.45%     |
+// 1768 -   Runtime: 53ms, beats 84.23%     |
+// 9    -   Runtime: 111ms, beats 96.99%    |   Memory: 57.71mb, beats 86.31%
+// 345  -                                   |   Memory: 55.78%, beats 75.41%
+// 151  -   Runtime: 51ms, beats 91.30%     |
+// 238  -   Runtime: 91ms, beats 94.68%     |
